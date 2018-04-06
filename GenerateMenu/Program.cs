5591a83ad4b4ba183501b33a9818657d1de1f12e
G <ever3stmomo@gmail.com>
2018-04-06 00:14:14 -0400

    First commit - Parses menu1.xml, and generates node and path value combination

---
+++ GenerateMenu/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GenerateMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*XElement xelement1 = XElement.Load("../../menu1.xml");
+            var item1 = from nm in xelement1.Elements("item")
+                /*where (string)nm.Element("Sex") == "Female"#1#
+                select nm;
+            Console.WriteLine("item:");
+            foreach (XElement xEle in item1)
+                Console.WriteLine(xEle);*/
+
+            StringBuilder outString = new StringBuilder("");
+            Console.WriteLine("==================");
+            XElement xelement = XElement.Load("../../menu1.xml");
+            IEnumerable<XElement> items = xelement.Elements();
+            try
+            {
+                foreach (var item in items)
+                {
+                    outString.Append(item.Element("displayName").Value + ", " + item.Element("path").Attribute("value").Value + "\r\n");
+                    //outString.Append()
+                }
+
+                Console.WriteLine(outString.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception encountered. Details:\r\n\t" + e.Message);
+                return;
+            }
+
+            
+
+        }
+    }
+}
